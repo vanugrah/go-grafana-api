96716694736cde07d2e36a5789181cd5f8978678
Anugrah Vijay <avijay@medallia.com>
2018-09-05 14:12:40 -0700

    implements GetAllFolders

--- error.go
+++ error.go
@@ -1,19 +1,5 @@
 package gapi
 
-type ErrNotFound struct {
-	Msg string
-}
-
-func (e *ErrNotFound) Error() string {
-	return e.Msg
-}
-
-func NewErrNotFound(msg string) error {
-	return &ErrNotFound {
-		Msg: msg,
-	}
-}
-
 // A GrafanaMessage contains the json error message received when http request failed
 type GrafanaErrorMessage struct {
 	Message string `json:"message"`
--- folders.go
+++ folders.go
@@ -1,8 +1,8 @@
 package gapi
 
 import (
-	"bytes"
 	"encoding/json"
+	"time"
 
 	"fmt"
 	"io/ioutil"
@@ -10,98 +10,119 @@ import (
 	"github.com/pkg/errors"
 )
 
-
-type FolderCreateResponse struct {
-	Id        int    `json:"id"`
-	uid       string `json:"uid"`
-	title     string `json:"title"`
-	url       string `json:"url"`
-	hasAcl    bool   `json:"hasAcl"`
-	canSave   bool   `json:"canSave"`
-	canEdit   bool   `json:"canEdit"`
-	canAdmin  bool   `json:"canAdmin"`
-	createdBy string `json:"createdBy"`
-	created   string `json:"created"`
-	updatedBy string `json:"updatedBy"`
-	updated	  string `json:"updated"`
-	version   int    `json:"version"`
+type Folder struct {
+	Id        int       `json:"id"`
+	Uid       string    `json:"uid"`
+	Title     string    `json:"title"`
+	Url       string    `json:"url"`
+	HasAcl    bool      `json:"hasAcl"`
+	CanSave   bool      `json:"canSave"`
+	CanEdit   bool      `json:"canEdit"`
+	CanAdmin  bool      `json:"canAdmin"`
+	CreatedBy string    `json:"createdBy"`
+	Created   time.Time `json:"created"`
+	UpdatedBy string    `json:"updatedBy"`
+	Updated   time.Time `json:"updated"`
+	Version   int       `json:"version"`
 }
 
-type Folder struct {
+type FolderCreateOpts struct {
 	Title string `json:"title"`
 	Id    int    `json:"id"`
 }
 
-func (c *Client) CreateFolder(model map[string]interface{}) (*FolderCreateResponse, error) {
-	wrapper := map[string]interface{}{
-		"title": model["title"],
-		"uid": 	 model["uid"],
-	}
-	data, err := json.Marshal(wrapper)
+func (c *Client) GetAllFolders() ([]Folder, error) {
+	folders := make([]Folder, 0)
+	req, err := c.newRequest("GET", "/api/folders/", nil, nil)
 	if err != nil {
-		return nil, errors.Wrap(err, "Failed to marshall folder JSON")
-	}
-	req, err := c.newRequest("POST", "/api/folders", nil, bytes.NewBuffer(data))
-	if err != nil {
-		return nil, err
+		return folders, err
 	}
 
 	resp, err := c.Do(req)
 	if err != nil {
-		return nil, errors.Wrap(err, "Unable to perform HTTP request")
+		return folders, errors.Wrap(err, "Unable to perform HTTP request")
 	}
 
 	if resp.StatusCode != 200 {
 		var gmsg GrafanaErrorMessage
 		dec := json.NewDecoder(resp.Body)
 		dec.Decode(&gmsg)
-		return nil, fmt.Errorf("Request to Grafana returned %+v status code with the following message: %+v", resp.StatusCode, gmsg.Message)
-	}
-
-	data, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	result := &FolderCreateResponse{}
-	err = json.Unmarshal(data, &result)
-	return result, err
-}
-
-func (c *Client) GetFolderByUID(slug string) (*Folder, error) {
-	path := fmt.Sprintf("/api/folders/%s", slug)
-	req, err := c.newRequest("GET", path, nil, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := c.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode != 200 {
-		var gmsg GrafanaErrorMessage
-		dec := json.NewDecoder(resp.Body)
-		dec.Decode(&gmsg)
-		errMsg := fmt.Sprintf("Request to Grafana returned %+v status code with the following message: %+v", resp.StatusCode, gmsg.Message)
-
-		// TODO: Give support to other custom errors
-		switch resp.StatusCode {
-		case 404:
-			return nil, NewErrNotFound(errMsg)
-		default:
-			return nil, fmt.Errorf(errMsg)
-		}
+		return folders, fmt.Errorf("Request to Grafana returned %+v status code with the following message: %+v", resp.StatusCode, gmsg.Message)
 	}
 
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return folders, err
 	}
 
-	result := &Folder{}
-	err = json.Unmarshal(data, &result)
-	return result, err
+	err = json.Unmarshal(data, &folders)
+	if err != nil {
+		return folders, err
+	}
+	return folders, err
 }
 
+// func (c *Client) CreateFolder(model map[string]interface{}) (*FolderCreateResponse, error) {
+// 	wrapper := map[string]interface{}{
+// 		"title": model["title"],
+// 		"uid":   model["uid"],
+// 	}
+// 	data, err := json.Marshal(wrapper)
+// 	if err != nil {
+// 		return nil, errors.Wrap(err, "Failed to marshall folder JSON")
+// 	}
+// 	req, err := c.newRequest("POST", "/api/folders", nil, bytes.NewBuffer(data))
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	resp, err := c.Do(req)
+// 	if err != nil {
+// 		return nil, errors.Wrap(err, "Unable to perform HTTP request")
+// 	}
+
+// 	if resp.StatusCode != 200 {
+// 		var gmsg GrafanaErrorMessage
+// 		dec := json.NewDecoder(resp.Body)
+// 		dec.Decode(&gmsg)
+// 		return nil, fmt.Errorf("Request to Grafana returned %+v status code with the following message: %+v", resp.StatusCode, gmsg.Message)
+// 	}
+
+// 	data, err = ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	result := &FolderCreateResponse{}
+// 	err = json.Unmarshal(data, &result)
+// 	return result, err
+// }
+
+// func (c *Client) GetFolderByUID(slug string) (*Folder, error) {
+// 	path := fmt.Sprintf("/api/folders/%s", slug)
+// 	req, err := c.newRequest("GET", path, nil, nil)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	resp, err := c.Do(req)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	if resp.StatusCode != 200 {
+// 		var gmsg GrafanaErrorMessage
+// 		dec := json.NewDecoder(resp.Body)
+// 		dec.Decode(&gmsg)
+// 		errMsg := fmt.Sprintf("Request to Grafana returned %+v status code with the following message: %+v", resp.StatusCode, gmsg.Message)
+// 	}
+
+// 	data, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	result := &Folder{}
+// 	err = json.Unmarshal(data, &result)
+// 	return result, err
+// }
